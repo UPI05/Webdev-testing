035051590524aad3909755ffad8feacbfca7ff97
Hieu V <upi05@kali>
2022-01-18 09:10:39 +0700

    test pret

--- server.js
+++ server.js
@@ -1,58 +1,58 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const path = require("path");
+const path = require('path')
 
-const api = require("./src/api");
+const api = require('./src/api')
 
-const morgan = require("morgan");
+const morgan = require('morgan')
 
-app.listen("3000", () => {
-  console.log("Runing");
-});
+app.listen('3000', () => {
+  console.log('Runing')
+})
 
 app.use((req, res, next) => {
-  console.log("1");
-  next();
-  console.log("2");
-  next();
-});
+  console.log('1')
+  next()
+  console.log('2')
+  next()
+})
 // Next() chuyen toi middleware tiep theo, middleware nao chay roi se ko chay lai
 app.use((req, res, next) => {
-  console.log("3");
-  next();
-  console.log("4");
-});
+  console.log('3')
+  next()
+  console.log('4')
+})
 
 const hello = (req, res, next) => {
-  console.log("Hieu V.");
+  console.log('Hieu V.')
   // Without next(), bellow middlewares won't be run
-  next();
-};
+  next()
+}
 
-app.use(hello);
+app.use(hello)
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(morgan("combined"));
+app.use(morgan('combined'))
 
 // Cac middleware sau nay se ko co next nen chi 1 trong cac truong hop dc chay
-app.use("/api/v1", api);
+app.use('/api/v1', api)
 
 app.use(
-  "/api/v2",
+  '/api/v2',
   (req, res, next) => {
-    req.hieuvMsg = "Api v2 is developing";
-    next();
+    req.hieuvMsg = 'Api v2 is developing'
+    next()
   },
   (req, res, next) => {
-    console.log(req.hieuvMsg);
-    next();
-  }
-);
+    console.log(req.hieuvMsg)
+    next()
+  },
+)
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "/package.json"));
-});
+  res.status(404).sendFile(path.join(__dirname, '/package.json'))
+})
 
-app.use((error, req, res, next) => {});
+app.use((error, req, res, next) => {})
